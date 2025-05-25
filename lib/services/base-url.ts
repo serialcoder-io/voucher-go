a2dceca5347e950c70e1ec4d48b1caf1ffa7794d
Omar.py <omaranli56@gmail.com>
2025-05-25 14:52:00 +0400

    feat: added new file profile.ts in lib/services and created function updateUserProfile inside

--- lib/services/base-url.ts
+++ lib/services/base-url.ts
@@ -1,1 +1,1 @@
-export const baseUrl = "http://192.168.76.83:8000"
+export const baseUrl = "http://192.168.67.83:8000"
---
+++ lib/services/profile.ts
@@ -0,0 +1,38 @@
+import { baseUrl } from "./base-url";
+
+type UserProfileParams = {
+    first_name: string,
+    last_name: string,
+    username: string
+    email: string
+}
+/**
+ * send a request to the endpoint and return the http status code
+ * if the status code is 200 it means user profile was update
+ * @param prams 
+ * @param access_token
+ * @returns the http status code
+ */
+export async function updateUserProfile(
+    params: UserProfileParams, 
+    accessToken: string
+): Promise<number>{
+    try {
+        const response = await fetch(`${baseUrl}/vms/auth/users/me/`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(params),
+        });
+        return response.status;
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            throw e;
+        } else {
+            throw new Error('Sorry, something went wrong: ' + JSON.stringify(e));
+        }
+    }
+}
